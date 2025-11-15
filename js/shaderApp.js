e8ce793d2bca52210e19db736ce4310270486501
Dawid <139803338+dawid-batkowski@users.noreply.github.com>
2025-11-15 23:00:45 +0100

    Update shaderApp.js

--- js/shaderApp.js
+++ js/shaderApp.js
@@ -3,3 +3,9 @@ uniforms = {
   u_mouse: { value: new THREE.Vector2() },
   u_resolution: { value: new THREE.Vector2(window.innerWidth, window.innerHeight) }
 };
+
+window.addEventListener('mousemove', (event) => {
+  // event.clientX / clientY gives pixel coordinates
+  uniforms.u_mouse.value.x = event.clientX;
+  uniforms.u_mouse.value.y = window.innerHeight - event.clientY; // flip Y axis
+});
