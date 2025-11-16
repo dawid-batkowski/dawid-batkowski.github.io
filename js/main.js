bb7947c7fc0e6b6fc1231eb2f9b8c182b130487b
dawidtrix <dawidtrix.trixit@gmail.com>
2025-11-16 20:13:44 +0100

    mobilefix

--- js/main.js
+++ js/main.js
@@ -59,12 +59,16 @@ function animate(time) {
 requestAnimationFrame(animate);
 
 // Handle window resize
-window.addEventListener("resize", () => {
-  const width = window.innerWidth;
-  const height = window.innerHeight;
-  renderer.setSize(width, height, false);
-  uniforms.u_resolution.value.set(width, height);
-});
+function resize() {
+  canvas.width = window.innerWidth * window.devicePixelRatio;
+  canvas.height = window.innerHeight * window.devicePixelRatio;
+
+  gl.viewport(0, 0, canvas.width, canvas.height);
+  shader.setUniform("u_resolution", [canvas.width, canvas.height]);
+}
+window.addEventListener("resize", resize);
+resize();
+
 
 
 window.addEventListener('mousemove', (event) => {
