be2015d40c075cf92f4ada9080ec6874e23b20e2
Dawid <dawidtrix.trixit@gmail.com>
2025-12-01 15:23:20 +0100

    smooth scroll more placeholders

---
+++ js/UX_Functionality.js
@@ -0,0 +1,21 @@
+console.log("Lenis script loaded");
+const lenis = new Lenis({
+    duration: 1.2,
+    easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+    direction: 'vertical',
+    gestureDirection: 'vertical',
+    smooth: true,
+    mouseMultiplier: 2,
+    smoothTouch: false,
+    touchMultiplier: 2,
+    infinite: false,
+  });
+  
+  // Get scroll progress
+  function raf(time) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+  }
+  
+  requestAnimationFrame(raf);
+  
