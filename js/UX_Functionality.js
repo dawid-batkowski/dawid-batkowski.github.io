5938c1a2eb3b6b0eba2cfcd4b2fc645313a645ae
Dawid <dawidtrix.trixit@gmail.com>
2025-12-09 17:38:20 +0100

    created breakdown template and fixed basic about section

--- js/UX_Functionality.js
+++ js/UX_Functionality.js
@@ -1,6 +1,6 @@
 console.log("Lenis script loaded");
 const lenis = new Lenis({
-    duration: 1.2,
+    duration: 0.75,
     easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
     direction: 'vertical',
     gestureDirection: 'vertical',
